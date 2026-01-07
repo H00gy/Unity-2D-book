d753a8c728d8c97d1b96ccc7360f0862f4e044e4
H00gy <dimitry.ross.hy@gmail.com>
2026-01-07 16:52:42 -0700

    half asleep will work on in 2 hour (thinking about table of contents)

--- Assets/scripts/TableOfContents.cs
+++ Assets/scripts/TableOfContents.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class TableOfContents : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform SpecificPage;
+    public Transform pages;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
