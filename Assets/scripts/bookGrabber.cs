1737cff865fb8318b8e0b8be0a996234221de8e9
H00gy <dimitry.ross.hy@gmail.com>
2026-01-08 14:02:47 -0700

    Update bookGrabber.cs

--- Assets/scripts/bookGrabber.cs
+++ Assets/scripts/bookGrabber.cs
@@ -2,18 +2,31 @@ using UnityEngine;
 
 public class bookGrabber : MonoBehaviour
 {
+    public GameObject closedBook;
+    public GameObject openBook;
+
+    private void Start()
+    {
+        closedBook.SetActive(false);
+    }
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("openBook"))
         {
-            Debug.Log("collided");
+            //Debug.Log("collided");
+            other.gameObject.SetActive(false);
+            closedBook.SetActive(true);
+
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
+        Vector2 resetPos = new Vector2(-2.23744011f, -2.19800019f);
         if (other.CompareTag("closedBook"))
         {
-            Debug.Log("left");
+            other.gameObject.SetActive(false);
+            other.transform.position = resetPos;
+
         }
     }
 }
