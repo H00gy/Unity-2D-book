bd7cf2261528384a7f33899298874493efd2652f
H00gy <dimitry.ross.hy@gmail.com>
2026-01-07 18:47:23 -0700

    Update TableOfContents.cs

--- Assets/scripts/TableOfContents.cs
+++ Assets/scripts/TableOfContents.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 public class TableOfContents : MonoBehaviour
 {
+    [Header("The Specific page to link to")]
     public Transform SpecificPage;
+    [Header("Page Transform")]
     public Transform pages;
+    [Header("PageHandler")]
+    public pageHandler pageHandler;
+    
 
+    public void onClick()
+    {
+        int index = SpecificPage.GetSiblingIndex();
+        pages.GetChild(0).gameObject.SetActive(false);
+        pages.GetChild(1).gameObject.SetActive(false);
+        pages.GetChild(index).gameObject.SetActive(true);
+        pages.GetChild(index + 1).gameObject.SetActive(true);
+        pageHandler.leftPageNum.text = (index + 1).ToString();
+        pageHandler.rightPageNum.text = (index + 2).ToString();
 
+    }
 }
